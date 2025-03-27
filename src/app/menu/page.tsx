dcd493cae6bb170b751030a5034b08ee764dd8a2
niaz abir <niazabir55@gmail.com>
2025-03-28 03:26:22 +0600

    add

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -120,7 +120,7 @@ const Menu = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 ml-4  gap-4 lg:ml-14">
+      <div className="grid grid-cols-1 lg:grid-cols-3 ml-8  gap-4 lg:ml-14">
         {/* {allFood?.map((food) => (
           <div key={food?.id}>
             <div className="w-full max-w-xs p-4 overflow-hidden rounded-lg ">
